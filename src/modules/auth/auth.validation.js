ed39f0d8000f39a8fd3adaeb9b9ad92275321c0d
mariam khaled <khaldmrym769@gmail.com>
2025-05-06 21:01:05 +0300

    edite auth

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -54,7 +54,7 @@ export const loginSchema = joi
         "string.pattern.base":
           "كلمة المرور يجب أن تحتوي على 8 أحرف على الأقل، حرف كبير، ورقم أو رمز خاص.",
       }),
-    jobs: joi.string().required().label("الوظيفة").messages(defaultMessages),
+    job: joi.string().required().label("الوظيفة").messages(defaultMessages),
   })
   .required();
 
