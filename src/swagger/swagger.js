553a6d1fc143b3163e3f7fd6a77b9edff13f9c8a
mariam khaled <khaldmrym769@gmail.com>
2025-05-19 23:34:34 +0300

    edie

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,16 +1,6 @@
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// اقرأ swagger.json فقط (لا تستخدم yaml هنا)
-const swaggerDocument = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8')
-);
+import swaggerDocument from './swagger.json' assert { type: 'json' };
 
 const router = Router();
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
