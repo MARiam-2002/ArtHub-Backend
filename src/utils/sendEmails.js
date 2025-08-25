37440d8d701f7363a946592387bfbbfb0c126490
mariam khaled <khaldmrym769@gmail.com>
2025-08-26 00:19:51 +0300

    first commit

--- src/utils/sendEmails.js
+++ src/utils/sendEmails.js
@@ -2,14 +2,18 @@ import nodemailer from 'nodemailer';
 
 export const sendEmail = async ({ to, subject, html, attachments }) => {
   const transporter = nodemailer.createTransport({
-    host: 'localhost',
+    service: 'gmail',
+    host: 'smtp.gmail.com',
     port: 465,
     secure: true,
-    service: 'gmail',
     auth: {
       user: process.env.EMAIL,
       pass: process.env.EMAIL_PASSWORD
-    }
+    },
+    tls: {
+      rejectUnauthorized: false
+    },
+    socketTimeout: 60000 // زيادة مهلة الاتصال إلى 60 ثانية
   });
 
   const mailOptions = {
